2d9d76188283162ca5cfd95f4c4f46f01d2760e1
mat-kuu <matildakanerva@outlook.com>
2022-06-02 14:30:24 +0200

    Creating  a repo on github Changing the name of default branch to main Clone a github repository

--- set-up-git.R
+++ set-up-git.R
@@ -17,3 +17,12 @@ git_vaccinate()
 
 #create a git repo
 usethis::use_git()
+
+#create a repo on GitHub
+use_github()
+
+#change the name of default branch
+git_default_branch_rename()
+
+#clone a github repository and fork it
+create_from_github("kingsleyshacklebolt/dragon_study")
