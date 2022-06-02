3ee3ea274847d0d57d39d8b388c0b237f38ef91b
mat-kuu <matildakanerva@outlook.com>
2022-06-02 15:21:57 +0200

    percentage of adelie penguins

--- example_script.R
+++ example_script.R
@@ -7,3 +7,13 @@ peng<-palmerpenguins::penguins
 
 #number of penguins
 total_penguins <-nrow(peng)
+
+#Adelie penguins
+adelie <- filter(peng, species == "Adelie")
+
+#percentage of adelie penguins from all
+(nrow(adelie)/total_penguins)*100
+
+
+
+
